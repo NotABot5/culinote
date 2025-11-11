c8cdeb8312e620d60f7b5abd593b962834f143cc
Aleksander Klik <aleksanderklik111@gmail.com>
2025-11-11 21:12:13 +0100

    Modified button to increase visibility

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,9 @@
 export default function Button({ onClick, children }) {
   return (
-    <button onClick={onClick} className="hover:bg-amber-200">
+    <button
+      onClick={onClick}
+      className="hover:bg-amber-200 p-1 border-2 rounded-md m-2"
+    >
       {children}
     </button>
   );
